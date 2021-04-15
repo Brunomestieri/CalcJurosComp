654130516ebda1099c1d875f5cb0d831c673fde8
Brunomestieri <44281678+Brunomestieri@users.noreply.github.com>
2021-04-15 19:40:27 -0300

    Add files via upload

---
+++ CalcJurosComp.py
@@ -0,0 +1,23 @@
+#coding: utf-8
+
+valorDeposito = input("Digite o valor do depósito \n")
+taxaJuros = input("Digite o valor da taxa de juros aplicada \n")
+dep = int(valorDeposito)
+taxa = int(taxaJuros)
+prazoAplicacao = input("Digite o prazo de resgate da aplicação \n")
+prazo = int(prazoAplicacao)
+porcento = taxa/100
+rend = dep*(1+porcento)**prazo
+
+print("=======================================================\n")
+
+print("Valor depositado: R$", valorDeposito)
+print("Taxa de juros aplicada: ", porcento, "%")
+print("Prazo da aplicação: ",prazo, "vezes\n")
+
+print("========================================================\n")
+
+print("O valor do rendimento será: R$", rend - dep)
+print("O valor total do investimento será: R$", rend, "\n")
+
+print("========================================================")
